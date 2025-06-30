45b31b52e1de842130232723cb31d3f263c330aa
donggonyoo <ddkk8525@gmail.com>
2025-06-30 17:39:33 +0900

    Bcrypt

--- springSecurity2/src/main/java/springSecurity2/config/SecurityConfig.java
+++ springSecurity2/src/main/java/springSecurity2/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.session.HttpSessionEventPublisher;
@@ -50,4 +51,7 @@ public class SecurityConfig {
     public BCryptPasswordEncoder bcryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public BCrypt bcrypt() {return  new BCrypt();}
 }
---
+++ springSecurity2/src/test/java/springSecurity2/TestBcrypt.java
@@ -0,0 +1,23 @@
+package springSecurity2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+
+public class TestBcrypt {
+    public static void main(String[] args) {
+        System.out.println("new BCryptPasswordEncoder().encode('123');");
+        String encode = new BCryptPasswordEncoder().encode("123");
+        System.out.println("123 -> : "+encode);
+
+        System.out.println("\n검증은 BCryptPasswordEncoder().matches(암호화x,암호화O)");
+        boolean matches1 = new BCryptPasswordEncoder().matches("123", encode);
+        boolean matches2 = new BCryptPasswordEncoder().matches("15523", encode);
+        System.out.println("123? : "+matches1);
+        System.out.println("15523? : "+matches2);
+
+
+    }
+
+}
